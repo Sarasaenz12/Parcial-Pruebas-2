56570ff5f204dc2aa8a2c427d114c933e1559d0b
Sarasaenz12 <svsanchez_223@cue.edu.co>
2026-06-05 17:57:42 -0500

    refactor: mejorar el codigo sobre el la validacion del monto

--- src/recarga.py
+++ src/recarga.py
@@ -1,7 +1,9 @@
 class MontoInvalidoError(Exception):
     pass
 
-
-def calcular_recarga(monto, premium=False):
+def _validar_monto(monto: int | float) -> None:
     if monto < 1000 or monto > 50000:
         raise MontoInvalidoError("El monto debe estar entre $1.000 y $50.000")
+
+def calcular_recarga(monto: int | float, premium: bool = False):
+    _validar_monto(monto)
